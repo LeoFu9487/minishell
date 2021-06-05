bf980d8168e50542ad67739c770094beedb1121b
LeoFu9487 <leofu890806@gmail.com>
2021-06-05 18:00:58 +0200

    new tester

--- test/builtin/echo/main.c
+++ test/builtin/echo/main.c
@@ -2,71 +2,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
-char **envp;
-
-void run_test(int test_id, char *arg[])
+int	main(int ac, char **av)
 {
-	if (fork())
-	{
-		wait(NULL);
-		return ;
-	}
-	char *test_file = ft_itoa(test_id);
-	char *ans_file = malloc((5 + ft_strlen(test_file)) * sizeof(char));
-	ans_file[0] = 'a';
-	ans_file[1] = 'n';
-	ans_file[2] = 's';
-	ans_file[3] = '_';
-	for (int i = 0 ; test_file[i] ; ++i){
-		ans_file[i + 4] = test_file[i];
-		ans_file[i + 5] = 0;
-	}
-	int fd_test = open(test_file, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	dup2(fd_test, STDOUT_FILENO);
-	builtin_echo(arg);
-	int fd_ans = open(ans_file, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	dup2(fd_ans, STDOUT_FILENO);
-	execve("/bin/echo", arg, envp);
-	free(test_file);
-	free(ans_file);
-}
-
-int	main(int ac, char **av, char **env)
-{
-	envp = env;
+	builtin_echo(av + 1);
 	(void)ac;
-	(void)av;
-	char *arg0[] = {"echo", "-n", "Hello World!", 0};
-	run_test(0, arg0);
-
-	char *arg1[] = {"echo", "Hello World!", 0};
-	run_test(1, arg1);
-
-	char *arg2[] = {"echo", "yoyo ", "-n", 0};
-	run_test(2, arg2);
-
-	char *arg3[] = {"echo", "-n", "-n", "-n", "OK", 0};
-	run_test(3, arg3);
-
-	char *arg4[] = {"echo", "-n ", "\\n", "-n", "OK", 0};
-	run_test(4, arg4);
-
-	char *arg5[] = {"echo", "-n ", "\n", "-n", "OK", 0};
-	run_test(5, arg5);
-
-	char *arg6[] = {"echo", "-ne", "-n", "haha", 0};
-	run_test(6, arg6);
-
-	char *arg7[] = {"echo", "-en", "haha", 0};
-	run_test(7, arg7);
-
-	char *arg8[] = {"echo", "-ne", "haha", 0};
-	run_test(8, arg8);
-
-	char *arg9[] = {"echo", "-nnnn", "haha", 0};
-	run_test(9, arg9);
-
-	char *arg10[] = {"echo", 0};
-	run_test(10, arg10);
 }
