31013324b93619197c0a21562c23e4cdf8c26ab1
LeoFu9487 <leofu890806@gmail.com>
2021-06-07 01:21:23 +0200

    turn off raw mode every get_input() finished

--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -6,7 +6,7 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 17:06:43 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/02 23:36:02 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/07 01:19:53 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ static void	handle_signal(int signal)
 	if (signal == SIGINT) /* ctrl + C */
 	{
 		ft_putstr_fd("SIGINT\n", 1);
+		raw_mode_switch(off);
 		exit(1);
 	}
 	if (signal == SIGQUIT) /* ctrl + \ */
 	{
 		ft_putstr_fd("SIGQUIT\n", 1);
+		raw_mode_switch(off);
 		exit(2);
 	}
 }
