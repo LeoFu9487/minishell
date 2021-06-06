613b2c8155b1c3f6e4051f07c8a9384b23511cba
LeoFu9487 <leofu890806@gmail.com>
2021-06-07 01:49:52 +0200

    start to do lexer

--- srcs/parse/lexer.c
+++ srcs/parse/lexer.c
@@ -6,7 +6,7 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 23:57:21 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/02 23:58:18 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/07 01:46:54 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 t_deque	*lexer(char *input_string)
 {
-	(void)input_string;
-	return (deque_init());
+	t_deque	*tokens;
+
+	tokens = deque_init();
+	deque_push_back(tokens, ft_strdup(input_string));
+	return (tokens);
 }
