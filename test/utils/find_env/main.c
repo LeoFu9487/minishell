fe4a97067b5210edbbd72372838fc4f409b6159e
LeoFu9487 <leofu890806@gmail.com>
2021-06-07 10:55:10 +0200

    change the prototype of find_env_var_line

--- test/utils/find_env/main.c
+++ test/utils/find_env/main.c
@@ -6,9 +6,11 @@
 
 int	main(int ac, char **av, char **env)
 {
+	t_double_list *it;
 	init_all(env);
 	if (ac < 2)
 		return (0);
-	ft_putendl_fd(find_env_var_line(av[1]), 1);
+	if ((it = find_env_var_line(av[1])))
+		ft_putendl_fd(it->content, 1);
 	ft_putendl_fd(find_env_var(av[1]), 1);
 }
