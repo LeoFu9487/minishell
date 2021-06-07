cbb19a10ee142a500f43f87d75eef99b765e1a7d
LeoFu9487 <leofu890806@gmail.com>
2021-06-07 08:19:55 +0200

    explanation of find_env* functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 13:52:33 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/07 08:10:39 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/07 08:19:27 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,9 +67,12 @@ void	catch_signal(void);//todo
 char	*find_env_var_line(char *key);//return the whole line, don't free after calling this function
 char	*find_env_var(char *key);//return the value that corresponds to the key, ft_free after the use of this function
 /*
+** return NULL if not foudn (for both of them)
+**
 ** example : 
 ** char *str = find_env_var_line("SHLVL");
 ** ft_putendl_fd(str, 1); //output : SHLVL=2
+** 
 ** char *str2 = find_env_var("SHLVL");
 ** ft_putendl_fd(str2, 1); // output : 2
 ** ft_free(str2); // need to free after used
