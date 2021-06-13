0e6203c6293185818b9c67399b18a44d21bab6ff
LeoFu9487 <leofu890806@gmail.com>
2021-06-13 23:07:24 +0200

    close stdout for exit in pipe

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 01:00:11 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/11 12:38:43 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/13 23:07:02 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	launch_builtins(char **args)
 		builtin_env(args);
 	//if (ft_strncmp(args[0], "exit", 5) == 0)
 	//{
+	//	close(STDOUT_FILENO);
 	//	builtin_exit(args);
 	//	message_exit(g_data.exit_status, "", -1);
 	//}
