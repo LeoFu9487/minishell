cd6a0c4eda0b2843eb1f883b2cb8c12de975ec1b
LeoFu9487 <leofu890806@gmail.com>
2021-06-14 18:59:48 +0200

    main_loop.c norm ok

--- srcs/main_loop.c
+++ srcs/main_loop.c
@@ -6,13 +6,42 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 17:54:40 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/12 21:38:05 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/14 18:56:25 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
- void	main_loop(void)
+static void	no_empty_process(char *input_string, t_deque *tokens)
+{
+	if (ft_strncmp(input_string, g_data.history->tail->content,
+			ft_strlen(input_string) + 1) != 0)
+		deque_push_back(g_data.history, input_string);
+	parse_and_execute(tokens);
+}
+
+static void	empty_process(char *input_string)
+{
+	if (g_data.lexer_error != NoError)
+	{
+		g_data.exit_status = 2;
+		if (ft_strncmp(input_string, g_data.history->tail->content,
+				ft_strlen(input_string) + 1) != 0)
+			deque_push_back(g_data.history, input_string);
+		ft_putstr_fd("minishell: unexpected EOL while looking for matching `",
+			2);
+		if (g_data.lexer_error == dquote)
+			ft_putendl_fd("\"\'", 2);
+		else if (g_data.lexer_error == quote)
+			ft_putendl_fd("\'\'", 2);
+		else
+			ft_putendl_fd("\\\'", 2);
+	}
+	else
+		ft_free(input_string);
+}
+
+void	main_loop(void)
 {
 	char	*input_string;
 	t_deque	*tokens;
@@ -26,25 +55,9 @@
 		raw_mode_switch(off);
 		tokens = lexer(input_string);
 		if (tokens->size > 0)
-		{
-			if (ft_strncmp(input_string, g_data.history->tail->content, ft_strlen(input_string)) != 0)
-				deque_push_back(g_data.history, input_string); // when it's written in history, don't free input_string
-			parse_and_execute(tokens);
-		}
+			no_empty_process(input_string, tokens);
 		else
-		{
-			ft_free(input_string);
-			if (g_data.lexer_error != NoError)
-			{
-				g_data.exit_status = 2;
-				if (g_data.lexer_error == dquote)
-					ft_putendl_fd("minishell: unexpected EOL while looking for matching `\"\'", 2);
-				else if (g_data.lexer_error == quote)
-					ft_putendl_fd("minishell: unexpected EOL while looking for matching `\'\'", 2);
-				else
-					ft_putendl_fd("minishell: unexpected EOL while looking for matching `\\\'", 2);
-			}
-		}
+			empty_process(input_string);
 		deque_clear(tokens, free_token);
 	}
 }
