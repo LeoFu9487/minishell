481e912535fa3c65ff4a56338700a28faf2dcab5
LeoFu9487 <leofu890806@gmail.com>
2021-06-15 03:45:50 +0200

    parse norm ok

--- srcs/parse/create_pipe.c
+++ srcs/parse/create_pipe.c
@@ -6,16 +6,15 @@
 /*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 23:09:38 by yfu               #+#    #+#             */
-/*   Updated: 2021/06/15 00:03:43 by yfu              ###   ########lyon.fr   */
+/*   Updated: 2021/06/15 03:28:04 by yfu              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	create_pipe_sub_1(t_deque *cmd_list, int size, t_iofd *iofd)
+static void	create_pipe_sub_1(t_deque *cmd_list, int size, t_iofd *iofd, int i)
 {
 	t_double_list	*iterator[3];
-	int				i;
 
 	iterator[0] = cmd_list->head;
 	i = -1;
@@ -26,7 +25,8 @@ static void	create_pipe_sub_1(t_deque *cmd_list, int size, t_iofd *iofd)
 		{
 			if (is_redir(((t_token *)iterator[1]->content)->lexer_flag))
 			{
-				set_redir(((t_token *)iterator[1]->content)->str, ((t_token *)iterator[1]->next->content)->str, &iofd[i]);
+				set_redir(((t_token *)iterator[1]->content)->str,
+					((t_token *)iterator[1]->next->content)->str, &iofd[i]);
 				iterator[2] = iterator[1];
 				iterator[1] = iterator[1]->next;
 				deque_pop_one(iterator[0]->content, iterator[2], free_token);
@@ -49,7 +49,35 @@ static void	create_pipe_sub_2(int **pipefd, int size)
 	while (++i < size - 1)
 	{
 		pipefd[i] = ft_calloc(2, sizeof(int));
-		if (pipe(pipefd[i]) < 0) message_exit(87, "pipe", 2);
+		if (pipe(pipefd[i]) < 0)
+			message_exit(87, "pipe", 2);
+	}
+}
+
+static void	create_pipe_sub_3(t_deque *cmd_list, int **pipefd, pid_t *pid,
+t_iofd *iofd)
+{
+	int	size;
+	int	i;
+
+	size = cmd_list->size;
+	i = size;
+	while (--i >= 0)
+	{
+		pid[i] = fork();
+		if (pid[i] < 0)
+			message_exit(87, "fork", 2);
+		else if (pid[i] > 0)
+		{
+			if (i == size - 1)
+				g_data.pid = pid[i];
+			if (i == 0)
+				close_pipefd(size, pipefd);
+			deque_clear(cmd_list->tail->content, free_token);
+			deque_pop_back(cmd_list, NULL);
+		}
+		else
+			create_pipe_child(cmd_list, size, pipefd, iofd);
 	}
 }
 
@@ -90,7 +118,7 @@ void	create_pipe(t_deque *cmd_list)
 
 	size = cmd_list->size;
 	iofd = init_iofd(size);
-	create_pipe_sub_1(cmd_list, size, iofd);
+	create_pipe_sub_1(cmd_list, size, iofd, -1);
 	if (cmd_list->size == 1)
 	{
 		no_pipe_command(cmd_list->head->content, &iofd[0]);
@@ -102,154 +130,6 @@ void	create_pipe(t_deque *cmd_list)
 	pipefd = ft_malloc(size - 1, sizeof(int *));
 	pid = ft_malloc(size, sizeof(pid_t));
 	create_pipe_sub_2(pipefd, size);
-
-
-
-	
-	pid[size - 1] = fork();
-	if (pid[size - 1] < 0)
-		message_exit(87, "fork", 2);
-	else if (pid[size - 1] > 0)
-	{
-		g_data.pid = pid[size - 1];
-		deque_clear(cmd_list->tail->content, free_token);
-		deque_pop_back(cmd_list, NULL);
-	}
-	else
-	{
-		dup2(pipefd[size - 2][0], STDIN_FILENO);
-		dup2(g_data.stdout_fd, STDOUT_FILENO);
-		if (iofd[size - 1].stdin_fd < 0)
-		{
-			ft_putstr_fd("minishell: ", 2);
-			if (ft_strncmp(iofd[size - 1].in_file, "", 1) == 0)
-				ft_putstr_fd(": ", 2);
-			perror(iofd[size - 1].in_file);
-			message_exit(1, "", -1);
-		}
-		else if (iofd[size - 1].stdin_fd != STDIN_FILENO)
-		{
-			dup2(iofd[size - 1].stdin_fd, STDIN_FILENO);
-			close(iofd[size - 1].stdin_fd);
-		}
-		if (iofd[size - 1].stdout_fd < 0)
-		{
-			ft_putstr_fd("minishell: ", 2);
-			if (ft_strncmp(iofd[size - 1].out_file, "", 1) == 0)
-				ft_putstr_fd(": ", 2);
-			perror(iofd[size - 1].out_file);
-			message_exit(1, "", -1);
-		}
-		else if (iofd[size - 1].stdout_fd != STDOUT_FILENO)
-		{
-			dup2(iofd[size - 1].stdout_fd, STDOUT_FILENO);
-			close(iofd[size - 1].stdout_fd);
-		}
-		for (int i = 0 ; i < size - 1 ; ++i)
-		{
-			close(pipefd[i][0]);
-			close(pipefd[i][1]);
-		}
-		run_command(cmd_list->tail->content);
-	}
-	for (int i = size - 2; i > 0; --i)
-	{
-		pid[i] = fork();
-		if (pid[i] < 0)
-			message_exit(87, "fork", 2);
-		else if (pid[i] > 0)
-		{
-			deque_clear(cmd_list->tail->content, free_token);
-			deque_pop_back(cmd_list, NULL);
-		}
-		else
-		{
-			dup2(pipefd[i - 1][0], STDIN_FILENO);
-			dup2(pipefd[i][1], STDOUT_FILENO);
-			if (iofd[i].stdin_fd < 0)
-			{
-				ft_putstr_fd("minishell: ", 2);
-				if (ft_strncmp(iofd[i].in_file, "", 1) == 0)
-					ft_putstr_fd(": ", 2);
-				perror(iofd[i].in_file);
-				message_exit(1, "", -1);
-			}
-			else if (iofd[i].stdin_fd != STDIN_FILENO)
-			{
-				dup2(iofd[i].stdin_fd, STDIN_FILENO);
-				close(iofd[i].stdin_fd);
-			}
-			if (iofd[i].stdout_fd < 0)
-			{
-				ft_putstr_fd("minishell: ", 2);
-				if (ft_strncmp(iofd[i].out_file, "", 1) == 0)
-					ft_putstr_fd(": ", 2);
-				perror(iofd[i].out_file);
-				message_exit(1, "", -1);
-			}
-			else if (iofd[i].stdout_fd != STDOUT_FILENO)
-			{
-				dup2(iofd[i].stdout_fd, STDOUT_FILENO);
-				close(iofd[i].stdout_fd);
-			}
-			for (int j = 0 ; j < size - 1 ; ++j)
-			{
-				close(pipefd[j][0]);
-				close(pipefd[j][1]);
-			}
-			run_command(cmd_list->tail->content);
-		}
-	}
-	pid[0] = fork();
-	if (pid[0] < 0)
-		message_exit(87, "fork", 2);
-	else if (pid[0] > 0)
-	{
-		for (int i = 0 ; i < size - 1 ; ++i)
-		{
-			close(pipefd[i][0]);
-			close(pipefd[i][1]);
-		}
-		deque_clear(cmd_list->tail->content, free_token);
-		deque_pop_back(cmd_list, NULL);
-	}
-	else
-	{
-		dup2(g_data.stdin_fd, STDIN_FILENO);
-		dup2(pipefd[0][1], STDOUT_FILENO);
-		if (iofd[0].stdin_fd < 0)
-		{
-			ft_putstr_fd("minishell: ", 2);
-			if (ft_strncmp(iofd[0].in_file, "", 1) == 0)
-				ft_putstr_fd(": ", 2);
-			perror(iofd[0].in_file);
-			message_exit(1, "", -1);
-		}
-		else if (iofd[0].stdin_fd != STDIN_FILENO)
-		{
-			dup2(iofd[0].stdin_fd, STDIN_FILENO);
-			close(iofd[0].stdin_fd);
-		}
-		if (iofd[0].stdout_fd < 0)
-		{
-			ft_putstr_fd("minishell: ", 2);
-			if (ft_strncmp(iofd[0].out_file, "", 1) == 0)
-				ft_putstr_fd(": ", 2);
-			perror(iofd[0].out_file);
-			message_exit(1, "", -1);
-		}
-		else if (iofd[0].stdout_fd != STDOUT_FILENO)
-		{
-			dup2(iofd[0].stdout_fd, STDOUT_FILENO);
-			close(iofd[0].stdout_fd);
-		}
-		for (int i = 0 ; i < size - 1 ; ++i)
-		{
-			close(pipefd[i][0]);
-			close(pipefd[i][1]);
-		}
-		run_command(cmd_list->tail->content);
-	}
-	
+	create_pipe_sub_3(cmd_list, pipefd, pid, iofd);
 	create_pipe_sub_4(size, pid, pipefd, iofd);
 }
---
+++ srcs/parse/create_pipe_sub.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_pipe_sub.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/15 02:34:39 by yfu               #+#    #+#             */
+/*   Updated: 2021/06/15 03:43:18 by yfu              ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	close_pipefd(int size, int **pipefd)
+{
+	int	i;
+
+	i = -1;
+	while (++i < size - 1)
+	{
+		close(pipefd[i][0]);
+		close(pipefd[i][1]);
+	}
+}
+
+static void	create_pipe_child_sub(int i, t_iofd *iofd)
+{
+	if (iofd[i].stdin_fd < 0 || iofd[i].stdout_fd < 0)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		if ((iofd[i].stdin_fd < 0 && ft_strncmp(iofd[i].in_file, "", 1) == 0)
+			|| (iofd[i].stdout_fd < 0
+				&& ft_strncmp(iofd[i].out_file, "", 1) == 0))
+			ft_putstr_fd(": ", 2);
+		if (iofd[i].stdin_fd < 0)
+			perror(iofd[i].in_file);
+		else
+			perror(iofd[i].out_file);
+		message_exit(1, "", -1);
+	}
+	if (iofd[i].stdin_fd != STDIN_FILENO)
+	{
+		dup2(iofd[i].stdin_fd, STDIN_FILENO);
+		close(iofd[i].stdin_fd);
+	}
+	if (iofd[i].stdout_fd != STDOUT_FILENO)
+	{
+		dup2(iofd[i].stdout_fd, STDOUT_FILENO);
+		close(iofd[i].stdout_fd);
+	}
+}
+
+void	create_pipe_child(t_deque *cmd_list, int size, int **pipefd,
+t_iofd *iofd)
+{
+	int	i;
+
+	i = cmd_list->size - 1;
+	if (i == 0)
+		dup2(g_data.stdin_fd, STDIN_FILENO);
+	else
+		dup2(pipefd[i - 1][0], STDIN_FILENO);
+	if (i == size - 1)
+		dup2(g_data.stdin_fd, STDOUT_FILENO);
+	else
+		dup2(pipefd[i][1], STDOUT_FILENO);
+	create_pipe_child_sub(i, iofd);
+	close_pipefd(size, pipefd);
+	run_command(cmd_list->tail->content);
+}
